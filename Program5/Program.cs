85e9b67ef0a60e70ff8f1e1fecde99e055da1906
coco0715 <ment0715@naver.com>
2023-10-02 20:51:02 +0900

    Feat: 2-2 별 찍기
    피라미드 출력까지 완성

---
+++ Program5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace SpecialLecture
+{
+    class Program5
+    {
+        static void Main(string[] args) {
+            // 2-1 구구단 출력하기
+            int i = 2;
+            int j = 1;
+            while(i <= 9)
+            {
+               while(j <= 9)
+                {
+                    Console.WriteLine($"{i} x {j} = {i * j}");
+                    j++;
+                }
+                j = 1;
+                i++;
+            }
+        }
+    }
+}
---
+++ Program7/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace SpecialLecture
+{
+    class Program7
+    {
+        static void Main(string[] args)
+        {
+            //2-3 최대값, 최소값 찾기
+
+        }
+    }
+}
